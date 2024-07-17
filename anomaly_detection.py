79d73f1b8724b8c6fc14ab3e43d6e23ea098d07e
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-07-17 11:56:52 +0530

    Update anomaly_detection.py

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -2,3 +2,36 @@
 ### Authenticator
 #---------------------------------------------------------------------------------------------------------------------------------
 import streamlit as st
+#---------------------------------------------------------------------------------------------------------------------------------
+### Import Libraries
+#---------------------------------------------------------------------------------------------------------------------------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#----------------------------------------
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.cluster import DBSCAN
+from scipy.stats import zscore
+#---------------------------------------------------------------------------------------------------------------------------------
+### Title and description for your Streamlit app
+#---------------------------------------------------------------------------------------------------------------------------------
+#import custom_style()
+st.set_page_config(page_title="Anomaly Detection",
+                   layout="wide",
+                   #page_icon=               
+                   initial_sidebar_state="collapsed")
+#----------------------------------------
+st.title(f""":rainbow[Anomaly Detection | v0.1]""")
+st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
+            unsafe_allow_html=True)
+#t.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
+#----------------------------------------
+# Set the background image
+st.divider()
+
+#---------------------------------------------------------------------------------------------------------------------------------
+### Functions & Definitions
+#---------------------------------------------------------------------------------------------------------------------------------
